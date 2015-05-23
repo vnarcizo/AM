809b5c7c606b33eabe0b1efc258b5b81fde02ac1
Luiz Gavioli <luizbag@gmail.com>
2015-05-23 11:22:39 -0300

    Comeando o treinamento

--- RNA/redeNeuralClassificar.m
+++ RNA/redeNeuralClassificar.m
@@ -2,7 +2,8 @@ function [ saida ] = redeNeuralClassificar(entrada, theta )
 
     a0 = [];
     a1 = [];
-    saida = zeros(size(entrada,1),1);
+    ns = size(theta{size(theta,1)},2);
+    saida = zeros(size(entrada,1),ns);
     for i = 1:size(entrada,1)
         a0 = [1, entrada(i,:)];
         for j = 1:size(theta,1)
@@ -10,6 +11,8 @@ function [ saida ] = redeNeuralClassificar(entrada, theta )
             a1 = sigmf(t,[1 0]);
             a0 = [1, a1];
         end
-        saida(i)=a1;
+        saida(i,:)=a1;
     end
+    saida(saida>=0.5)=1;
+    saida(saida<0.5)=0;
 end
--- RNA/redeNeuralTreinamento.m
+++ RNA/redeNeuralTreinamento.m
@@ -1,9 +1,19 @@
 function [ theta ] = redeNeuralTreinamento( entrada, saida, nCamadas, nNeuronios )
     
     theta = cell(nCamadas);
-    theta{1} = randn(size(entrada,2)+1, size(entrada,2));
+    theta{1} = randn(size(entrada,2)+1, nNeuronios);
     for i = 2:nCamadas-1
         theta{i} = randn(nNeuronios+1, nNeuronios);
     end
     theta{nCamadas} = randn(nNeuronios+1, size(saida,2));
+    
+    for i = 1:size(entrada,1)
+        x = entrada(i,:);
+        y = redeNeuralClassificar(x, theta);
+        dl = saida(i,:)-y);
+        for j = 1:nCamadas
+            l = nCamadas-j;
+            dl = 
+        end
+    end
 end
