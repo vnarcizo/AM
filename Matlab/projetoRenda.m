f2ecc2589ce5ba2e5ef832ad1a275a16b7d8c9a8
Luiz Gavioli <luizbag@gmail.com>
2015-06-09 18:15:12 -0300

    while para perguntar de novo se a pessoa quer rodar mais algum método

--- Matlab/projetoRenda.m
+++ Matlab/projetoRenda.m
@@ -68,175 +68,179 @@ dadosAparticionarNaiveBayes =  horzcat(dadosNaiveBayes, rotulosNormalizados);
 
 % size(dadosParticionados)
 % size(dadosNaiveBayesParticionados)
-
+metodoClassificacao = 0;
 %% Seleção do métodos
+while metodoClassificacao ~= 6
+    fprintf('0 - Todos\n')
+    fprintf('1 - KNN\n')
+    fprintf('2 - Regressão logística\n')
+    fprintf('3 - Redes Neurais Artificiais\n')
+    fprintf('4 - SVM\n')
+    fprintf('5 - Naive Bayes\n')
+    fprintf('6 - Sair\n')
 
-fprintf('0 - Todos\n')
-fprintf('1 - KNN\n')
-fprintf('2 - Regressão logística\n')
-fprintf('3 - Redes Neurais Artificiais\n')
-fprintf('4 - SVM\n')
-fprintf('5 - Naive Bayes\n')
-
-metodoClassificacao = input('Selecione o método que deseja executar\n');
-
-hipotesesRegressao = cell(numeroParticoes);
-avaliacoesRegressao = [];
-avaliacoesRNA = [];
-hipoteseCarregada = [];
-
-modelosSVM = cell(numeroParticoes);
-avaliacoesSVM = [];
-
-avaliacoesKnn = [];
-
-modelosNB = cell(numeroParticoes);
-avaliacoesNaiveBayes = [];
-
-if metodoClassificacao == 0 || metodoClassificacao == 1
-    k = input('Qual o valor de K? (Número de vizinhos mais próximos): \n');
-end
-
-%% Selecoes de parametros adicionais para Regressao Logistica
-if metodoClassificacao == 0 || metodoClassificacao == 2
-    fprintf('1 - Hipótese linear\n')
-    fprintf('2 - Hipótese quadrática\n')
-    fprintf('3 - Hipótese cúbica\n')
-    hipoteseRegressao = input('Selecione a hipótese desejada\n');
-    
-    carregarHipotese = input('Carregar hipotese previamente calculada? (S/N)\n', 's');
-    
-    lambda = 0;
-    
-    if strcmpi(carregarHipotese, 'S')
-        load(strcat('HipoteseRegressao_',hipoteseRegressao,'.mat'));
-    else
-        utilizarRegularizacao = input('Utilizar regularização? (S/N)\n', 's');
-
-        if (strcmpi(utilizarRegularizacao,'S'))
-            lambda = input('Qual o valor do parâmetro de regularização?\n');
-        end
+    metodoClassificacao = input('Selecione o método que deseja executar\n');
+    if metodoClassificacao == 6
+        break
     end
-    
-end
 
-%Redes Neurais
-if metodoClassificacao == 0 || metodoClassificacao == 3
-    carregarThetas = input('Carregar os Thetas previamente calculados? (S/N)\n', 's');
-    
-    if (strcmpi(carregarThetas,'S'))
-            load('thetasRedesNeurais.mat');
-    end
-end
+    hipotesesRegressao = cell(numeroParticoes);
+    avaliacoesRegressao = [];
+    avaliacoesRNA = [];
+    hipoteseCarregada = [];
 
-%% Classificação
-for i = 1:numeroParticoes
-    indicesTreinamento = 1:numeroParticoes;
-    indicesTreinamento = indicesTreinamento(indicesTreinamento~=i);
-    
-    dadosTreinamento = dadosParticionados(indicesTreinamento,:,:);
-    dadosTreinamento = reshape(dadosTreinamento, size(dadosTreinamento, 1)*size(dadosTreinamento, 2), size(dadosTreinamento, 3));
-    dadosTeste = dadosParticionados(i,:,:);
-    dadosTeste = squeeze(dadosTeste);
-    
-    rotulosTreinamento = dadosTreinamento(:,end);
-    atributosTreinamento = dadosTreinamento(:,1:end-1);
+    modelosSVM = cell(numeroParticoes);
+    avaliacoesSVM = [];
 
-    rotulosTeste = dadosTeste(:, end);
-    atributosTeste = dadosTeste(:, 1:end-1);
+    avaliacoesKnn = [];
+
+    modelosNB = cell(numeroParticoes);
+    avaliacoesNaiveBayes = [];
 
     if metodoClassificacao == 0 || metodoClassificacao == 1
-        avaliacaoKnn = knn(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste, k, i);
-        avaliacoesKnn = vertcat(avaliacoesKnn, avaliacaoKnn);
+        k = input('Qual o valor de K? (Número de vizinhos mais próximos): \n');
     end
-    % Regressão Logística
+
+    %% Selecoes de parametros adicionais para Regressao Logistica
     if metodoClassificacao == 0 || metodoClassificacao == 2
-        
-         if (strcmpi(carregarHipotese,'N'))
-                    [ avaliacao, hipotesesRegressao{i}] = ...
-                    regressaoLogistica(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste,...
-                    hipoteseRegressao, utilizarRegularizacao, lambda, i, 0 );   
-         else
-             switch hipoteseRegressao
-                    case 1
-                        atributosTesteExpandidos = atributosTeste;
-                    case 2
-                        atributosTesteExpandidos = RL_expandeAtributosPolinomial(atributosTeste, 2);
-                    case 3
-                        atributosTesteExpandidos = RL_expandeAtributosPolinomial(atributosTeste, 3);
-             end
-             
-                valorPrevistoTeste = RL_predicao(melhorHipoteseRegressao, atributosTesteExpandidos);
-                avaliacao = avaliar(valorPrevistoTeste,valorPrevistoTeste);
-         end
-                      
-           avaliacoesRegressao = vertcat(avaliacoesRegressao, avaliacao);
+        fprintf('1 - Hipótese linear\n')
+        fprintf('2 - Hipótese quadrática\n')
+        fprintf('3 - Hipótese cúbica\n')
+        hipoteseRegressao = input('Selecione a hipótese desejada\n');
+
+        carregarHipotese = input('Carregar hipotese previamente calculada? (S/N)\n', 's');
+
+        lambda = 0;
+
+        if strcmpi(carregarHipotese, 'S')
+            load(strcat('HipoteseRegressao_',hipoteseRegressao,'.mat'));
+        else
+            utilizarRegularizacao = input('Utilizar regularização? (S/N)\n', 's');
+
+            if (strcmpi(utilizarRegularizacao,'S'))
+                lambda = input('Qual o valor do parâmetro de regularização?\n');
+            end
+        end
+
     end
+
+    %Redes Neurais
     if metodoClassificacao == 0 || metodoClassificacao == 3
-        
-         if (strcmpi(carregarThetas,'N'))
-           [mTheta1, mTheta2, avaliacao] = RNA_treinamento(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste,i,200,100);
-         else
-             rTesteItem = zeros(size(atributosTeste,1),1);
-              for item = 1:size(atributosTeste,1)
-                  rTesteItem(item) =  RNA_forward(atributosTeste(item,:),mTheta1, mTheta2);
-              end
-              
-                acuraciaTeste = mean(double(rotulosTeste == rTesteItem)) * 100;
-                fprintf('Acuracia na base de teste: %f na partição %d\n', acuraciaTeste, i);
-                avaliacao = avaliar(rTesteItem,rotulosTeste);
-         end
-          
-         avaliacoesRNA = vertcat(avaliacoesRNA, avaliacao);
+        carregarThetas = input('Carregar os Thetas previamente calculados? (S/N)\n', 's');
+
+        if (strcmpi(carregarThetas,'S'))
+                load('thetasRedesNeurais.mat');
+        end
     end
+
+    %% Classificação
+    for i = 1:numeroParticoes
+        indicesTreinamento = 1:numeroParticoes;
+        indicesTreinamento = indicesTreinamento(indicesTreinamento~=i);
+
+        dadosTreinamento = dadosParticionados(indicesTreinamento,:,:);
+        dadosTreinamento = reshape(dadosTreinamento, size(dadosTreinamento, 1)*size(dadosTreinamento, 2), size(dadosTreinamento, 3));
+        dadosTeste = dadosParticionados(i,:,:);
+        dadosTeste = squeeze(dadosTeste);
+
+        rotulosTreinamento = dadosTreinamento(:,end);
+        atributosTreinamento = dadosTreinamento(:,1:end-1);
+
+        rotulosTeste = dadosTeste(:, end);
+        atributosTeste = dadosTeste(:, 1:end-1);
+
+        if metodoClassificacao == 0 || metodoClassificacao == 1
+            avaliacaoKnn = knn(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste, k, i);
+            avaliacoesKnn = vertcat(avaliacoesKnn, avaliacaoKnn);
+        end
+        % Regressão Logística
+        if metodoClassificacao == 0 || metodoClassificacao == 2
+
+             if (strcmpi(carregarHipotese,'N'))
+                        [ avaliacao, hipotesesRegressao{i}] = ...
+                        regressaoLogistica(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste,...
+                        hipoteseRegressao, utilizarRegularizacao, lambda, i, 0 );   
+             else
+                 switch hipoteseRegressao
+                        case 1
+                            atributosTesteExpandidos = atributosTeste;
+                        case 2
+                            atributosTesteExpandidos = RL_expandeAtributosPolinomial(atributosTeste, 2);
+                        case 3
+                            atributosTesteExpandidos = RL_expandeAtributosPolinomial(atributosTeste, 3);
+                 end
+
+                    valorPrevistoTeste = RL_predicao(melhorHipoteseRegressao, atributosTesteExpandidos);
+                    avaliacao = avaliar(valorPrevistoTeste,valorPrevistoTeste);
+             end
+
+               avaliacoesRegressao = vertcat(avaliacoesRegressao, avaliacao);
+        end
+        if metodoClassificacao == 0 || metodoClassificacao == 3
+
+             if (strcmpi(carregarThetas,'N'))
+               [mTheta1, mTheta2, avaliacao] = RNA_treinamento(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste,i,200,100);
+             else
+                 rTesteItem = zeros(size(atributosTeste,1),1);
+                  for item = 1:size(atributosTeste,1)
+                      rTesteItem(item) =  RNA_forward(atributosTeste(item,:),mTheta1, mTheta2);
+                  end
+
+                    acuraciaTeste = mean(double(rotulosTeste == rTesteItem)) * 100;
+                    fprintf('Acuracia na base de teste: %f na partição %d\n', acuraciaTeste, i);
+                    avaliacao = avaliar(rTesteItem,rotulosTeste);
+             end
+
+             avaliacoesRNA = vertcat(avaliacoesRNA, avaliacao);
+        end
+        if metodoClassificacao == 0 || metodoClassificacao == 4
+            [avaliacao, modelosSVM{i}] = svm(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste, i);
+        end
+        if metodoClassificacao == 0 || metodoClassificacao == 5
+              dadosTreinamentoNB = dadosNaiveBayesParticionados(indicesTreinamento,:,:);
+              dadosTreinamentoNB = reshape(dadosTreinamentoNB, size(dadosTreinamentoNB, 1)*size(dadosTreinamentoNB, 2), size(dadosTreinamentoNB, 3));
+
+              dadosTesteNB = squeeze(dadosNaiveBayesParticionados(i,:,:));
+
+              rotulosTreinamentoNB = dadosTreinamentoNB(:,end);
+              atributosTreinamentoNB = dadosTreinamentoNB(:,1:end-1);
+
+              rotulosTesteNB = dadosTesteNB(:, end);
+              atributosTesteNB = dadosTesteNB(:, 1:end-1);
+
+            [avaliacao, modelosNB{i}] = naiveBayes(atributosTreinamentoNB, rotulosTreinamento, atributosTesteNB, rotulosTeste, i);
+        end
+
+    end
+
+    if metodoClassificacao == 0 || metodoClassificacao == 1
+        fprintf('Resultados KNN\n');
+        avaliarFinal(avaliacoesKnn);
+    end
+
+    if metodoClassificacao == 0 || metodoClassificacao == 2
+        fprintf('Resultados regressão logística\n');
+        indiceMelhorHipotese = avaliarFinal(avaliacoesRegressao);
+
+        exportarRegresao = input('Deseja exportar a melhor hipotese? (S/N)\n', 's');
+        if (strcmpi(exportarRegresao,'S'))
+            melhorHipoteseRegressao = hipotesesRegressao{indiceMelhorHipotese};
+            save(strcat('HipoteseRegressao_', num2str(hipoteseRegressao), ' .mat'), 'melhorHipoteseRegressao','hipoteseRegressao');
+        end   
+    end
+
     if metodoClassificacao == 0 || metodoClassificacao == 4
-        [avaliacao, modelosSVM{i}] = svm(atributosTreinamento, rotulosTreinamento, atributosTeste, rotulosTeste, i);
+        fprintf('Resultados SVM \n');
+        indiceMelhorModelo = avaliarFinal(avaliacoesSVM);
+
+        exportarSVM = input('Deseja exportar o modelo do SVM? (S/N)\n', 's');
+
+        if (strcmpi(exportarSVM,'S'))
+            melhorModeloSVM = modelosSVM{indiceMelhorHipotese};
+            save('ModeloSVM.mat', 'melhorModeloSVM');
+        end   
     end
-    if metodoClassificacao == 0 || metodoClassificacao == 5
-          dadosTreinamentoNB = dadosNaiveBayesParticionados(indicesTreinamento,:,:);
-          dadosTreinamentoNB = reshape(dadosTreinamentoNB, size(dadosTreinamentoNB, 1)*size(dadosTreinamentoNB, 2), size(dadosTreinamentoNB, 3));
-    
-          dadosTesteNB = squeeze(dadosNaiveBayesParticionados(i,:,:));
-    
-          rotulosTreinamentoNB = dadosTreinamentoNB(:,end);
-          atributosTreinamentoNB = dadosTreinamentoNB(:,1:end-1);
-
-          rotulosTesteNB = dadosTesteNB(:, end);
-          atributosTesteNB = dadosTesteNB(:, 1:end-1);
-            
-        [avaliacao, modelosNB{i}] = naiveBayes(atributosTreinamentoNB, rotulosTreinamento, atributosTesteNB, rotulosTeste, i);
-    end
-   
 end
-
-if metodoClassificacao == 0 || metodoClassificacao == 1
-    fprintf('Resultados KNN\n');
-    avaliarFinal(avaliacoesKnn);
-end
-
-if metodoClassificacao == 0 || metodoClassificacao == 2
-    fprintf('Resultados regressão logística\n');
-    indiceMelhorHipotese = avaliarFinal(avaliacoesRegressao);
-    
-    exportarRegresao = input('Deseja exportar a melhor hipotese? (S/N)\n', 's');
-    if (strcmpi(exportarRegresao,'S'))
-        melhorHipoteseRegressao = hipotesesRegressao{indiceMelhorHipotese};
-        save(strcat('HipoteseRegressao_', num2str(hipoteseRegressao), ' .mat'), 'melhorHipoteseRegressao','hipoteseRegressao');
-    end   
-end
-
-if metodoClassificacao == 0 || metodoClassificacao == 4
-    fprintf('Resultados SVM \n');
-    indiceMelhorModelo = avaliarFinal(avaliacoesSVM);
-    
-    exportarSVM = input('Deseja exportar o modelo do SVM? (S/N)\n', 's');
-    
-    if (strcmpi(exportarSVM,'S'))
-        melhorModeloSVM = modelosSVM{indiceMelhorHipotese};
-        save('ModeloSVM.mat', 'melhorModeloSVM');
-    end   
-end
-
 
 %% Finalizacao
 %clear; %Descomentar na versao final
