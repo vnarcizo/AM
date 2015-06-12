4654f2923627799f6bf2cba831b42fa2ff73b0e1
Luiz Gavioli <luizbag@gmail.com>
2015-06-12 09:35:14 -0300

    Perguntando se deseja plotar a base

--- Matlab/projetoRenda.m
+++ Matlab/projetoRenda.m
@@ -40,18 +40,8 @@ fprintf('Pré-processando iniciado...\n\n');
 [dadosNaiveBayes] = preProcessarNaiveBayes(dadosPreprocessados, indiceNumericos);
 
 %% ================= Parte 3: Normalização dos dados ====================
-% O programa solicita qual normalização deseja efetuar, se por Escala ou
-% Padronização
-
-tipoNormalizacao = input('Deseja normalizar por Escala ou Padronização? (E/P) \n', 's');
-
-if(strcmpi(tipoNormalizacao, 'E'))
-    fprintf('Normalização por escala iniciada...\n\n');
-    [dadosNormalizados] = normalizarEscala(dadosPreprocessados);
-else
-    fprintf('Normalização por padronização iniciada...\n\n');
-    [dadosNormalizados] = normalizarPadronizacao(dadosPreprocessados);
-end
+fprintf('Normalização por padronização iniciada...\n\n');
+[dadosNormalizados] = normalizarPadronizacao(dadosPreprocessados);
 
 rotulosNormalizados = rotulos;
 
@@ -65,20 +55,25 @@ rotulosNormalizados(linhasAusentes, :) = [];
 
 dadosNormalizados(:, colunasAusentes) = [];
 
-Z = reduzir_atributos(dadosNormalizados, 3);
+plotar = input('Deseja visualizar o espalhamento dos dados (dimensão reduzida)? (S/N)\n', 's');
 
-pos = find(rotulosNormalizados == 1);
-neg = find(rotulosNormalizados == 0);
-figure; hold on;
-namostras = 5000;
-scatter3(Z(pos(1:namostras), 1), Z(pos(1:namostras), 2), Z(pos(1:namostras), 3), 'b+');
-scatter3(Z(neg(1:namostras), 1), Z(neg(1:namostras), 2), Z(neg(1:namostras), 3), 'ro');
-legend('>=50k','<50k');
+if strcmpi(plotar, 'S')
+    Z = reduzir_atributos(dadosNormalizados, 3);
 
-%plot(Z(neg(1:10000), 1), Z(neg(1:10000), 2), 'ko', 'MarkerFaceColor', 'r', 'MarkerSize', 7);
-%plot(Z(pos(1:10000), 1), Z(pos(1:10000), 2), 'b+','LineWidth', 2, 'MarkerSize', 7);
-title('Plot 3D da base de dados');
-hold off;
+    pos = find(rotulosNormalizados == 1);
+    neg = find(rotulosNormalizados == 0);
+    figure; hold on;
+    namostras = 5000;
+    scatter3(Z(pos(1:namostras), 1), Z(pos(1:namostras), 2), Z(pos(1:namostras), 3), 'b+');
+    scatter3(Z(neg(1:namostras), 1), Z(neg(1:namostras), 2), Z(neg(1:namostras), 3), 'ro');
+    legend('>=50k','<50k');
+
+    %plot(Z(neg(1:10000), 1), Z(neg(1:10000), 2), 'ko', 'MarkerFaceColor', 'r', 'MarkerSize', 7);
+    %plot(Z(pos(1:10000), 1), Z(pos(1:10000), 2), 'b+','LineWidth', 2, 'MarkerSize', 7);
+    title('Plot 3D da base de dados');
+    hold off;
+    pause;
+end
 dadosNaiveBayes(linhasAusentes, :) = [];
 dadosNaiveBayes(:, union(colunasAusentes, indiceNumericos)) = [];
 
